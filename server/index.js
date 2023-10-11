78d61ead007c531e8c1eca0174e3c2309689e92a
Fabián Andrés Henry Vilaxa <113631396+Fhv75@users.noreply.github.com>
2023-10-11 00:09:24 -0300

    Create index.js

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const path = require('path')
+const userRouter = require('./routes/userRoutes')
+
+const app = express()
+
+dotenv.config({path: path.resolve(__dirname, '../.env') })
+const PORT = process.env.PORT || 5000
+
+const frontendURL = 'http://localhost:5173'
+
+app.use(cors({ origin: frontendURL}))
+app.use(express.json())
+app.use(userRouter)
+
+app.listen(PORT, () => {
+    console.log('app running on port ' + PORT)
+})
