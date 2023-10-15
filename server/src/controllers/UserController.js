566e9c5cd27f9660bdd55cd726cea3637cba8600
Fabián Andrés Henry Vilaxa <113631396+Fhv75@users.noreply.github.com>
2023-10-15 02:23:42 -0300

    Added login

--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -1,4 +1,5 @@
 const getDatabaseConnection = require("../db/conn")
+const { validationResult } = require('express-validator');
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
@@ -7,14 +8,17 @@ class UserController {
     constructor() {
         this.db = getDatabaseConnection()
         this.createUser = this.createUser.bind(this)
+        this.login = this.login.bind(this)
     }
 
     async createUser(req, res) {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
         try {
             const { correo_electronico, nombre, rut, contraseña, telefono } = req.body
-            if (!correo_electronico || !nombre || !rut || !contraseña || !telefono) {
-                return res.status(400).json({ message: 'Datos incompletos' })
-            }
 
             const hashedPw = await bcrypt.hash(contraseña, 10)
 
@@ -34,6 +38,43 @@ class UserController {
         } catch (error) {
             console.log(error.message)
             res.status(500).json({ message: 'Error al crear el usuario', error: error.message })
+        }
+    }
+
+    async login(req, res) {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        try {
+            const { correo_electronico, contraseña } = req.body
+
+            const user = await this.db.usuario.findUnique({
+                where: {
+                    correo_electronico: correo_electronico
+                },
+            })
+
+            const validPw = await bcrypt.compare(contraseña, user?.contrase_a)
+
+            if (validPw) {
+                const token = jwt.sign({
+                    correo_electronico: user?.correo_electronico,
+                    userId: user?.id,
+                    rol: user?.rol
+                },
+                    process.env.JWT_KEY,
+                    { expiresIn: '6h' }
+                )
+                return res.status(201).json({ token, userId: user?.id })
+            } else {
+                return res.status(401).json({
+                    message: 'Falló la autenticación'
+                })
+            }
+        } catch (error) {
+            res.status(500).json({ message: 'Error al iniciar sesión', error: error.message })
         }
     }
 }
